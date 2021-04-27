274ad603409d883094848cd65aaf8b929bad993b
korenshir <67112374+korenshir@users.noreply.github.com>
2021-04-27 13:03:26 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
-import react from 'react'
-import React,{useEffect,useState} from 'react'
+import React,{useEffect} from 'react'
 import sanityClient from "./../client.js";
 import BlockContent from "@sanity/block-content-to-react";
 
@@ -21,7 +20,7 @@ export default function About() {
    <main className="relative" style={{fontFamily:"Quicksand"}}>
        <div className="p-20 lg:pt-20 container mx-auto relative">
          <section className="lg:flex p-5" style={{borderRadius:"5%"}}>
-             <img src={author.photoLink} style={{borderRadius:"40%"}}className="rounded w-22 h-22 lg:w-32 lg:h-32 mr-20 mt-20"/>
+             <img src={author.photoLink} style={{borderRadius:"40%"}} alt="author image" className="rounded w-22 h-22 lg:w-32 lg:h-32 mr-20 mt-20"/>
              <div className="text-lg flex flex-col justufy-center">
                  <h1 className="cursive text-6xl text-green-900 mb-4">
                      Hey there, I'm{" "}
