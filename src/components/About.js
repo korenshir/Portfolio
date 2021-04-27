cee9c00a609ccaa51f9ef7b05834c2fb604130fd
korenshir <67112374+korenshir@users.noreply.github.com>
2021-04-27 12:37:13 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import react from 'react'
 import React,{useEffect,useState} from 'react'
 import sanityClient from "./../client.js";
 import BlockContent from "@sanity/block-content-to-react";
-import background from "./../backgrounds/IMG-1251123.jpg";
+import background from "./../backgrounds/aboutme.JPG";
 
 export default function About() {
     const [author,setAuthor]= React.useState(null);
@@ -20,17 +20,16 @@ export default function About() {
         <div>
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand"></link>
    <main className="relative" style={{fontFamily:"Quicksand"}}>
-       <img src={background} height='300px' className="absolute w-full"/>
        <div className="p-20 lg:pt-20 container mx-auto relative">
-         <section className="bg-green-700 rounded-lg shadow-2xl lg:flex p-5">
-             <img src={author.photoLink} style={{borderRadius:"40%"}}className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/>
+         <section className="lg:flex p-5" style={{borderRadius:"5%"}}>
+             <img src={author.photoLink} style={{borderRadius:"40%"}}className="rounded w-22 h-22 lg:w-32 lg:h-32 mr-20 mt-20"/>
              <div className="text-lg flex flex-col justufy-center">
-                 <h1 className="cursive text-6xl text-green-300 mb-4">
+                 <h1 className="cursive text-6xl text-green-900 mb-4">
                      Hey there, I'm{" "}
-                     <span className="text-green-100">{author.name}
+                     <span className="text-green-700">{author.name}
                      </span>
                  </h1>
-                 <div className="prose lg:prose-xl text-white" style={{fontSize:"16px"}}>
+                 <div className="prose lg:prose-xl text-black" style={{fontSize:"16px"}}>
                     <BlockContent blocks={author.bio} projectId="g520sv3c" dataset="production"/>
                  </div>
              </div>
